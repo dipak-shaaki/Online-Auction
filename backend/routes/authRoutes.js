68ce96849167f70962f3620df4e333273d84d0dc
Dipak Shaaki <shanki.dipak@gmail.com>
2025-06-17 20:39:57 +0545

    Phase 3: Authentication(Register, Login)

---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const { check } = require('express-validator');
+const { register, login } = require('../controllers/authController');
+
+const router = express.Router();
+
+router.post('/register', [
+  check('name', 'Name is required').not().isEmpty(),
+  check('email', 'Please include a valid email').isEmail(),
+  check('password', 'Password should be 6+ chars').isLength({ min: 6 }),
+], register);
+
+router.post('/login', login);
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const authRoutes = require('./routes/authRoutes');
+app.use('/api/auth', authRoutes);
+
+
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.error(err));
